34733c8e70a43d34445159422aef7080f8c186bf
Vetek Bence <vebence@gmail.com>
2022-11-16 15:22:59 +0100

    edge

--- network/js/main.js
+++ network/js/main.js
@@ -57,7 +57,7 @@ function initSigma(config) {
         defaultHoverLabelBGColor: "#002147",
         defaultLabelHoverColor: "#fff",
         labelThreshold: 10,
-        defaultEdgeType: "curve",
+        defaultEdgeType: "straight",
         hoverFontStyle: "bold",
         fontStyle: "bold",
         activeFontStyle: "bold"
